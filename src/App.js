6a6cbd6775ac08d93c417ec97556e7eae0f29905
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2022-07-19 10:01:36 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+// Router
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// Components
+import Home from './components/Home'
+import News from './components/News'
+import Donate from './components/Donate'
+import Gallery from './components/Gallery'
+
+class App extends Component {
+  render () {
+	return (
+		<Router>
+		 <div className="App">
+		  <Route exact path="/" render={HomeComponent}>
+  }
+}
+
+export default App;
